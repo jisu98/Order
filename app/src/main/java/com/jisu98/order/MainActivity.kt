3f83a09e47c90bcab67c7d2cbea0bfce05aa64ef
jisu <jisu98@korea.ac.kr>
2026-06-29 17:38:11 +0900

    feat(presentation): add navigation setup

--- app/src/main/java/com/jisu98/order/MainActivity.kt
+++ app/src/main/java/com/jisu98/order/MainActivity.kt
@@ -7,41 +7,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jisu98.order.presentation.menu.MenuScreen
+import com.jisu98.order.presentation.order.OrderScreen
 import com.jisu98.order.ui.theme.OrderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             OrderTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavHost(
+                        navController = navController,
+                        startDestination = "menu",
+                        modifier = Modifier.padding(innerPadding),
+                    ) {
+                        composable("menu") {
+                            MenuScreen(
+                                onNavigateToOrder = { navController.navigate("order") },
+                            )
+                        }
+                        composable("order") {
+                            OrderScreen(
+                                onOrderComplete = { navController.popBackStack() },
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OrderTheme {
-        Greeting("Android")
     }
 }
